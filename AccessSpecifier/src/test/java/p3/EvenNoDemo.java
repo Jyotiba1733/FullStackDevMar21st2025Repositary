e4298cdf3e548b40f29f7a5f00f91278cdb05727
Jyotiba1733 <jyotibabekwadakar@gmail.com>
2025-05-02 12:26:04 +0530

    Demo Even Program

---
+++ AccessSpecifier/src/test/java/p3/EvenNoDemo.java
@@ -0,0 +1,16 @@
+package p3;
+
+public class EvenNoDemo
+{
+    public static void main(String[] args) {
+
+        System.out.println("Even Numbers Between 1 to 50");
+        for(int i=0;i<=50;i++)
+        {
+            if(i%2==0)
+            {
+                System.out.println(i);
+            }
+        }
+    }
+}
